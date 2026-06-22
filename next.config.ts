6b176e9b2247b7295aed702969aa8310b013eb18
Shukladev7 <exit08006@gmail.com>
2026-06-22 11:55:09 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,19 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cdn.sanity.io",
       },
+      {
+        protocol: "https",
+        hostname: "pub-49184aefe88141419f93bc73dbf68688.r2.dev",
+        pathname: "/**",
+      },
     ],
     formats: ["image/avif", "image/webp"],
   },
+
   allowedDevOrigins: ["172.20.10.4"],
+
   compress: true,
+
   poweredByHeader: false,
 };
 
